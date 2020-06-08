503b82a7c8d273c5d7a6bb6820aed2a3afd80aa4
Hunter Danielson <hunterleedanielson@gmail.com>
2020-06-08 14:49:38 -0700

    all tests pass, refactor parseRequest

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,39 +6,48 @@ describe('createResponse', () => {
     return request(app)
       .get('/')
       .then(res => {
-        expect(res.text).toEqual('<h1>Hello</h1>');
+        expect(res.text).toEqual('hi');
       });
   });
 
   it('handles the /echo route', () => {
     return request(app)
-      .get('/')
+      .post('/echo')
+      .send('this is the body echo')
       .then(res => {
-        expect(res.text).toEqual('<h1>Hello</h1>');
+        expect(res.text).toEqual('this is the body echo');
       });
   });
 
   it('handles the /red route', () => {
     return request(app)
-      .get('/')
+      .get('/red')
       .then(res => {
-        expect(res.text).toEqual('<h1>Hello</h1>');
+        expect(res.text).toEqual('<h1>red</h1>');
       });
   });
 
   it('handles the /green route', () => {
     return request(app)
-      .get('/')
+      .get('/green')
       .then(res => {
-        expect(res.text).toEqual('<h1>Hello</h1>');
+        expect(res.text).toEqual('<h1>green</h1>');
       });
   });
 
   it('handles the /blue route', () => {
     return request(app)
-      .get('/')
+      .get('/blue')
       .then(res => {
-        expect(res.text).toEqual('<h1>Hello</h1>');
+        expect(res.text).toEqual('<h1>blue</h1>');
+      });
+  });
+
+  it('returns 404 for other routes', () => {
+    return request(app)
+      .get('/anythingelse')
+      .then(res => {
+        expect(res.text).toEqual('<h1>404 Not found</h1>');
       });
   });
 });
