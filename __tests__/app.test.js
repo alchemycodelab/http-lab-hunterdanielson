b7804c68177195fd2ded3d7d10587ec4a99e054b
Hunter Danielson <hunterleedanielson@gmail.com>
2020-06-08 13:22:43 -0700

    app test settup

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,43 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+  it('handles the / route', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('<h1>Hello</h1>');
+      });
+  });
 
+  it('handles the /echo route', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('<h1>Hello</h1>');
+      });
+  });
+
+  it('handles the /red route', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('<h1>Hello</h1>');
+      });
+  });
+
+  it('handles the /green route', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('<h1>Hello</h1>');
+      });
+  });
+
+  it('handles the /blue route', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('<h1>Hello</h1>');
+      });
+  });
 });
